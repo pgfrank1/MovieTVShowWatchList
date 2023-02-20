dfc281110760f129bbb279e5c7583507fec2772c
pgfrank1 <patrickfrank1995@gmail.com>
2023-02-20 16:58:58 -0600

    feat(entity): Added instance variables to the User class
    Added first name, last name, username, and id to the class's variables.
    
    Author: pgfrank1

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,57 @@
+package entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.*;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "username")
+    private String username;
+
+    public User() { }
+
+    public User(String firstName, String lastName, String username) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
