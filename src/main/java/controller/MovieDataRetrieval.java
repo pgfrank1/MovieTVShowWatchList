4f74a40d9fe8a6f71e8badd536f3d292b33bad59
pgfrank1 <patrickfrank1995@gmail.com>
2023-03-02 23:56:16 -0600

    feat(controller): MovieDataRetrieval is now implemented
    This class has the ability to get individual movie information and place that information into the session.
    
    Author: pgfrank1

--- src/main/java/controller/MovieDataRetrieval.java
+++ src/main/java/controller/MovieDataRetrieval.java
@@ -1,18 +1,47 @@
 package controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
 
 @WebServlet(
         name = "MovieDataRetrieval",
-        value = "/MovieDataRetrieval"
+        value = "/MovieInfo"
 )
 public class MovieDataRetrieval extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        String urlForward = "/individualMovieInfo.jsp";
+        HttpSession session = req.getSession();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        int movieId = Integer.parseInt(req.getParameter("movie_id"));
+        String movieInfoUrl =  "https://api.themoviedb.org/3/movie/" + movieId + "?api_key="
+                + getServletContext().getAttribute("apiKey") + "&language=en-US";
+
+        try {
+            URL urlMovieInfo = new URL(movieInfoUrl);
+            Map<String, Object> movieInfo = objectMapper.readValue(urlMovieInfo, new TypeReference<>() {});
+            session.setAttribute("movieInfo", movieInfo);
+        } catch (MalformedURLException e) {
+            logger.error("There was an error with forming the url for an individual movie.\n" + e);
+        } catch (IOException e) {
+            logger.error("An IO error has occurred when attempting to read data from the individual movie"
+                    + " API URL.\n" + e);
+        }
 
 
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(urlForward);
+        dispatcher.forward(req, resp);
     }
 }
