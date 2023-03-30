fe5237abdd901e2b62ce82647c4c53107cfcb591
pgfrank1 <patrickfrank1995@gmail.com>
2023-03-30 12:01:54 -0500

    fix(controller): Actually added the RESTfull Application class
    Author: pgfrank1

--- src/main/java/pgfrank/controller/RESTfulUsers.java
+++ src/main/java/pgfrank/controller/RESTfulUsers.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("IndieProject_war/users")
+@Path("/users")
 public class RESTfulUsers {
     @GET
     @Produces(MediaType.TEXT_HTML)
