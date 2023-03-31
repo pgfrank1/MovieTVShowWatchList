a544775de8cb906394f5a74353633309851e5f24
pgfrank1 <patrickfrank1995@gmail.com>
2023-03-30 21:58:31 -0500

    fix(controller): Auth now successfully checks the database
    Auth no longer places the username within the database even if the user already exists. Also doesn't add a duplicate.
    
    Author: pgfrank1

--- src/main/java/pgfrank/controller/Auth.java
+++ src/main/java/pgfrank/controller/Auth.java
@@ -1,6 +1,5 @@
 package pgfrank.controller;
 
-
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
@@ -10,9 +9,9 @@ import pgfrank.auth.*;
 import pgfrank.entity.user.User;
 import pgfrank.persistence.GenericDao;
 import pgfrank.util.PropertiesLoader;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.commons.io.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -21,7 +20,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.net.URI;
 import java.net.URL;
@@ -36,13 +36,15 @@ import java.security.PublicKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
-import java.util.*;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 
 @WebServlet(
-        name = "userAuth",
-        value = "/auth"
+        urlPatterns = {"/auth"}
 )
 // TODO if something goes wrong it this process, route to an error page. Currently, errors are only caught and logged.
 /**
@@ -71,7 +73,8 @@ public class Auth extends HttpServlet implements PropertiesLoader {
 
     /**
      * Gets the auth code from the request and exchanges it for a token containing user info.
-     * @param req servlet request
+     *
+     * @param req  servlet request
      * @param resp servlet response
      * @throws ServletException
      * @throws IOException
@@ -83,19 +86,16 @@ public class Auth extends HttpServlet implements PropertiesLoader {
         HttpSession session = req.getSession();
 
         if (authCode == null) {
-            RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
-            dispatcher.forward(req, resp);
+            //TODO forward to an error page or back to the login
         } else {
             HttpRequest authRequest = buildAuthRequest(authCode);
             try {
-
                 TokenResponse tokenResponse = getToken(authRequest);
                 DecodedJWT userInfo = validate(tokenResponse);
-                //String userName = jwt.getClaim("cognito:username").asString();
-                //String firstName = jwt.getClaim("given_name").asString();
                 session.setAttribute("userName", userInfo.getClaim("cognito:username").asString());
                 session.setAttribute("firstName", userInfo.getClaim("name").asString());
                 session.setAttribute("lastName", userInfo.getClaim("family_name").asString());
+                req.setAttribute("userName", userName);
             } catch (IOException e) {
                 logger.error("Error getting or validating the token: " + e.getMessage(), e);
                 //TODO forward to an error page
@@ -111,6 +111,7 @@ public class Auth extends HttpServlet implements PropertiesLoader {
 
     /**
      * Sends the request for a token to Cognito and maps the response
+     *
      * @param authRequest the request to the oauth2/token url in cognito
      * @return response from the oauth2/token endpoint which should include id token, access token and refresh token
      * @throws IOException
@@ -133,7 +134,6 @@ public class Auth extends HttpServlet implements PropertiesLoader {
         return tokenResponse;
 
     }
-
     /**
      * Get values out of the header to verify the token is legit. If it is legit, get the claims from it, such
      * as username.
@@ -181,25 +181,22 @@ public class Auth extends HttpServlet implements PropertiesLoader {
         String userName = jwt.getClaim("cognito:username").asString();
         logger.debug("here's the username: " + userName);
         logger.debug("here are all the available claims: " + jwt.getClaims());
+
         //TODO: Place this in it's own method. Checks if the user already exists in the database
         GenericDao<User> daoUsers = new GenericDao<>(User.class);
-        List<User> users = daoUsers.getAll();
-        for (User checkUser : users) {
-            if (checkUser.getUsername().equals(jwt.getClaim("cognito:username").asString())) {
-                break;
-            }
-            else {
-                User user = new User(jwt.getClaim("cognito:username").asString(), null,
-                        jwt.getClaim("name").asString(), jwt.getClaim("family_name").asString());
-                daoUsers.insertType(user);
-            }
+        List<User> users = daoUsers.getByPropertyValue("username", userName);
+        if (users.size() == 0) {
+            User newUser = new User(userName, null, jwt.getClaim("name").asString(),
+                    jwt.getClaim("family_name").asString());
+            daoUsers.insertType(newUser);
         }
 
-        // TODO decide what you want to do with the info!
+        // TODO decide what you want to do with the info!0
         return jwt;
     }
 
-    /** Create the auth url and use it to build the request.
+    /**
+     * Create the auth url and use it to build the request.
      *
      * @param authCode auth code received from Cognito as part of the login process
      * @return the constructed oauth request
@@ -229,7 +226,7 @@ public class Auth extends HttpServlet implements PropertiesLoader {
     /**
      * Gets the JSON Web Key Set (JWKS) for the user pool from cognito and loads it
      * into objects for easier use.
-     *
+     * <p>
      * JSON Web Key Set (JWKS) location: https://cognito-idp.{region}.amazonaws.com/{userPoolId}/.well-known/jwks.json
      * Demo url: https://cognito-idp.us-east-2.amazonaws.com/us-east-2_XaRYHsmKB/.well-known/jwks.json
      *
